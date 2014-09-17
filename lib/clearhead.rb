1f7dd895891fc7828e21394354a2476c65a2c37a
tomhanlon <tomhanlon@gmail.com>
2014-09-16 22:11:31 -0500

    Prompt for global css file

--- lib/clearhead.rb
+++ lib/clearhead.rb
@@ -16,34 +16,50 @@ module Clearhead
 
   Welcome to the Clearhead Experiment Generator. What is the name of your experiment?
   "
-  experiment_name = gets.chomp
-  FileUtils::mkdir_p "#{experiment_name}"
+  experiment_name = gets.chomp.downcase
+  FileUtils::mkdir_p "#{experiment_name}/css"
+  FileUtils::mkdir_p "#{experiment_name}/js"
   author_name = `git config user.name`
 
   puts "How many variations of this test will there be? Please enter a number."
   variation_count = gets.chomp.to_i
 
   puts "Please provide a brief description of the experiment."
-  description = gets.chomp.to_i
+  description = gets.chomp.downcase
+
+  puts "Do you need a global CSS file for this experiment?"
+  global_css = gets.chomp.downcase
+
+  if global_css == "yes"
+    File.write("#{experiment_name}/css/#{experiment_name}-v#{variation + 1}.css", "
+    /*--------------------------------------------
+     *
+     *  test: #{experiment_name}
+     *  description: #{description} - global-css
+     *  author: #{author_name}
+     *
+     * ------------------------------------------*/
+    ")
+  end
 
   variation_count.times do |variation|
 
     # Write js and css file per variation in  in experiment directory
-    File.write("#{experiment_name}/#{experiment_name}-v#{variation + 1}.js", "
+    File.write("#{experiment_name}/js/#{experiment_name}-v#{variation + 1}.js", "
     /*--------------------------------------------
      *
      *  test: #{experiment_name}
-     *  description: #{description} - v#{variation + 1}
+     *  description: #{description} - v#{variation + 1}.js
      *  author: #{author_name}
      *
      * ------------------------------------------*/
     ")
 
-    File.write("#{experiment_name}/#{experiment_name}-v#{variation + 1}.css", "
+    File.write("#{experiment_name}/css/#{experiment_name}-v#{variation + 1}.css", "
     /*--------------------------------------------
      *
      *  test: #{experiment_name}
-     *  description: #{description} - v#{variation + 1}
+     *  description: #{description} - v#{variation + 1}.css
      *  author: #{author_name}
      *
      * ------------------------------------------*/
